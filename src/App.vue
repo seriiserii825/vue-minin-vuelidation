a0987a0cd136d00ab02279f1862c2cebc3e9904f
serii <seriiburduja@gmail.com>
2020-10-12 16:56:29 +0300

    required

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,34 @@
     <form>
       <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" id="email" class="form-control">
+        <input
+            type="email"
+            id="email"
+            class="form-control"
+            @input="$v.email.$touch()"
+            v-model="email"
+        >
       </div>
+        <pre>
+          {{ $v }}
+        </pre>
     </form>
   </div>
 </template>
 
 <script>
+import {required} from 'vuelidate/lib/validators'
+
 export default {
   name: 'app',
   data() {
     return {
-      input: ''
+      email: ''
+    }
+  },
+  validations: {
+    email: {
+      required
     }
   }
 }
